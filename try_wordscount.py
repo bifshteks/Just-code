f4f98befb245533e6e512f79534bcdb643dbee4a
bifshteks <artem.pif@mail.ru>
2016-11-13 18:43:41 +0300

    stage 2

--- try_wordscount.py
+++ try_wordscount.py
@@ -1,14 +1,65 @@
-def print_word(filename):
-  def dict_count_word():
+#def dict_count_word(filename):
+#
+#  text = open(filename, 'r')
+#
+#  read_text = text.read()
+#  rext = read_text.split()
+#  for word in rext:      #why is don't work?
+#    word.lower()         #
+#  dict_1 = {word: rext.count(word) for word in rext}
+#
+#  text.close()
+#
 
-    text = open(filename, 'r')
+#def print_word(filename):
+# 
+#  dict_count_word()
+#  return (dict_1)
 
-    read_text = text.read()
-    rext = read_text.split()
-    for word in rext:      #why is don't work?
-      word.lower()         #
-    dict_1 = {word: rext.count(word) for word in rext}
 
-    text.close()
 
-  print (dict_1)
+
+
+#def print_top():
+filename = 'alice.txt'
+#dict_count_word()
+text = open(filename, 'r')
+
+read_text = text.read()
+rext = read_text.split()
+for word in rext:      #why it is don't work?
+  word.lower()         #
+dict_1 = {word: rext.count(word) for word in rext}
+text.close()
+
+list_value = list(dict_1.values())
+list_keys = list(dict_1.keys())
+#list_value_2 = list_value
+#list_value_2.sort()
+for i in range(20):
+  for index in list_value:
+    if index == max(list_value):
+      print(list_keys[index])
+      list_value.remove(index)
+
+   
+
+
+      
+
+
+  
+#def sth():
+#  for i in list_value:
+#    if i == max(list_value):
+#      print(dict_1.item(i))
+#      list_value.popitem(i)
+
+#list_top_values = list_value[-20:]   # Топ-20 сортированных значений из словаря
+#list_top_sort = 
+#print(list_value_2)
+#print(list_keys)
+  
+
+#print (list_value)
+#print (list_top_values)
