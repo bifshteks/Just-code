af46ac4a5f30828ecbcdba5cb1d1879e524839a6
bifshteks <artem.pif@mail.ru>
2017-03-06 11:35:42 +0300

    datelist gui ver 3.0

--- datelist.py
+++ datelist.py
@@ -16,6 +16,7 @@ m = (
         'Ноябрь',
         'Декабрь'
         )
+
 months = {
         'Январь': 1,
         'Февраль': 2,
@@ -42,46 +43,12 @@ weekday = {
             7: 'Воскресенье'
             }
 
-month = months[choicebox('Month:', 'Datelist', m)] # месяц цифрой
-day = enterbox('Day:', 'Datelist') # число без нуля в начале
-
-if day.startswith('0'): day = int(day[1:]) # yбираем ноль у day
-else:  day = int(day)
-
-# return (номер дня недели первого(пн - 0, вс - 6) , всего дней в месяце)
-weekday_alldays = monthrange(2017, month) # ниже
-
-wd = weekday_alldays[0] # день недели
-
-for i in range(1, day + 1):
-    wd += 1 if wd != 7 else -6
-
-# ищем номер данного дня в году:
-day_in_year = 0
-for i in range(1, month):
-    day_in_year += monthrange(2017, i)[1]
-day_in_year += day
- 
-
-def get_subj(week, num_week):
-    res = []
-    for time, dict_of_time in prac[week].items():
-        for subj, tupl_weeks in dict_of_time.items():
-            if num_week in tupl_weeks:
-                res.append(tuple([ time , subj ]))
-        
-    return res
-
-
-num_weekday = round( (day_in_year - 1 - wd ) / 7  + 2)
-
-
 prac = {
             'Понедельник': {'09:00 - 10:35': {'Физ - ра': (1,2,3,4,5,6,7,8,9)},
                             '10:55 - 12:30': {'Анатомия л': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)},
                             '13:10 - 16:35': {'Уход': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18)}},      # [ str(time) , {subject1: [1 ,3 ,5 ,8], subject2: [2, 4, 6]}]
             'Вторник': {'09:00 - 12:30': {'Биология': (1,3,5,7,9,11,13), 'Латинский': (2,4,6,8,10,12,14,15,16,17)},
-                        '13:10 - 14:45': {'Гиста': (1,2,3,4,5,6,7,8,9,10,11,12,13), 'Экономика': (14)},
+                        '13:10 - 14:45': {'Гиста': (1,2,3,4,5,6,7,8,9,10,11,12,13), 'Экономика': (14,)},
                         '13:10 - 16:35': {'Экономика' : (15,16,17)}},
             'Среда': { '10:55 - 12:30': {'Экономика': (1,3,5,7,9,11,13,15,17)},
                        '13:10 - 16:35' : {'Химия': (2,4,6,8,10,12,13,14,15,16,17), 'Физика': (1,3,5,7,9,11)}},
@@ -94,11 +61,73 @@ prac = {
             'Суббота': {'09:00 - 12:30': {'Ин. яз.': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17)},
                         '13:10 - 14:45': {'Физика': (1,3,5,7,9,11)},
                         '13:10 - 16:35': {'Анатомия': (6,)}},
-            'Воскресенье': {} 
+            'Воскресенье': {'00':{'Не учимся': (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22)}} 
             }
+def get_subj(week, num_week):
+    '''принимает неделю в месяце прописью и номер недели в году
+    Возвращает предметы в этот день'''
+    global prac
+    res = []
+    for time, dict_of_time in prac[week].items():
+        for subj, tupl_weeks in dict_of_time.items():
+            if num_week in tupl_weeks:
+                res.append(tuple([ time , subj ]))
             
-result = sorted(get_subj(weekday[wd], num_weekday), key=lambda t: int(t[0][:2]))
-msgbox('\n'.join([' :  '.join((str(k[0]), str(k[1]))) for k in result]), 'Раписание')
-if '__name__' == '__main__':
-    month = months[choicebox('Month:', 'Datelist', m)]
-    n = input()
+    return res
+
+def find_datelist(day, month):
+    global weekday
+    # return (номер дня недели первого(пн - 0, вс - 6) , всего дней в месяце)
+    weekday_alldays = monthrange(2017, month) # ниже
+
+    wd = weekday_alldays[0] # день недели
+
+    for i in range(1, day + 1):
+        wd += 1 if wd != 7 else -6
+
+    # ищем номер данного дня в году:
+    day_in_year = 0
+    for i in range(1, month):
+        day_in_year += monthrange(2017, i)[1]
+    day_in_year += day
+     
+    # деь была get_subj()
+
+
+
+    num_weekday = round( (day_in_year - 1 - wd ) / 7  + 2 - 6)### - 6
+                
+    result = sorted(get_subj(weekday[wd], num_weekday), key=lambda t: int(t[0][:2]))# [('time', subj), ...]
+    result.append(weekday[wd])
+    return result
+        
+def main():
+    month = months[choicebox('Month:', 'Datelist', m)] # месяц цифрой
+    day = enterbox('Day:\n (Можно указать несколько введя числа в ввиде "7-13", но не больше 10ти)', 'Datelist') # число без нуля в начале
+    days_to_print = []
+    if '-' in day:
+        day = day.split('-')
+        foo = [_ for _ in range(int(day[0]), int(day[1])+1)]
+        days_to_print.extend(foo)
+    else:
+        day = int(day)
+        days_to_print.append(day)
+
+    datelist = ''
+    for d in days_to_print:
+        day_datelist = find_datelist(d, month)
+        week = day_datelist[-1]
+        qwe = '\n\t'.join(
+                        [' : '.join([tpl[0], tpl[1]]) for tpl in day_datelist[:-1] ]
+                         )
+        datelist += '%s  %s\n' % (week, d) + \
+                    '\t' + qwe\
+                    + '\n\n' if qwe else\
+                    '%s  %s\n' % (week, d) + '\tНе учимся\n\n'
+    #print(datelist)
+    msgbox(datelist, 'Раписание')
+    while True:
+        main()
+    
+if __name__ == '__main__':
+    main()
